9c951976f44a2ffa747602e41b60f98a6d5cc33a
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-26 22:48:25 -0700

    Add basic App component

---
+++ client/src/components/App.jsx
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react'
+import baseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import BeatSequencer from './BeatSequencer';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+class App extends Component {
+  getChildContext() {
+    return { muiTheme: getMuiTheme(baseTheme) };
+  }
+
+  render() {
+    return(
+      <section>
+        <BeatSequencer />
+      </section>
+    );
+  }
+}
+
+App.childContextTypes = {
+  muiTheme: PropTypes.object.isRequired,
+};
+
+export default App;
