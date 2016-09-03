5c5d947a887c401aecf9bc18ef66996ba5efdd2d
Rafael Espinoza <nerddiffer@gmail.com>
2016-09-03 14:46:30 -0700

    Start using Provider, store, reducer from index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import rootReducer from './reducers';
 import App from './components/App'
 
-render(<App />, document.getElementById('app'));
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
