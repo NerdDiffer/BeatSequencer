d4704140a05dd1b19a377d40a9110f2c0ce8ed98
Rafael Espinoza <nerddiffer@gmail.com>
2016-09-04 23:44:23 -0700

    Playing with data for initial sequences

--- client/src/data.js
+++ client/src/data.js
@@ -5,15 +5,19 @@ export default [
     id: 0,
     tone: '200',
     soundDef: 'bell',
+    // events: [[1, 0], [0, 1], 1, 1],
     events: [1, 0, 0, 0],
+    // events: [1, 0, 0, 0],
     subdivision: '4n',
     mute: false
   },
   {
     id: 1,
-    tone: '200',
+    tone: '300',
     soundDef: 'conga',
-    events: [0, 1, 0, 0],
+    events: [[0, 0, 1, 1], [0, 0, 1, 1], [0, 0, 1, 1], [0, 0, 1, 1]],
+    // events: [0, 1, 0, 1],
+    // events: [0, 0, [0, 1], [0, 1]],
     subdivision: '4n',
     mute: false
   },
@@ -21,7 +25,8 @@ export default [
     id: 2,
     tone: 'Bb4',
     soundDef: 'membrane',
-    events: [0, 0, 1, 0],
+    // events: [0, 0, [0, 1], [0, 1]],
+    events: [0, 1, 0, 1],
     subdivision: '4n',
     mute: false
   },
@@ -29,7 +34,7 @@ export default [
     id: 3,
     tone: '200',
     soundDef: 'metal',
-    events: [0, 0, 0, 1],
+    events: [[0, 1], [0, 1], [0, 1], [0, 1]],
     subdivision: '4n',
     mute: false
   }
