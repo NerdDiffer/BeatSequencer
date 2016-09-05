f0854a7e4401e0b258d18e2c07c30e04bd3ec76b
Rafael Espinoza <nerddiffer@gmail.com>
2016-09-04 17:54:41 -0700

    Move data for initial sequences into separate file

---
+++ client/src/data.js
@@ -0,0 +1,36 @@
+// seed data
+
+export default [
+  {
+    id: 0,
+    tone: '200',
+    soundDef: 'bell',
+    events: [1, 0, 0, 0],
+    subdivision: '4n',
+    mute: false
+  },
+  {
+    id: 1,
+    tone: '200',
+    soundDef: 'conga',
+    events: [0, 1, 0, 0],
+    subdivision: '4n',
+    mute: false
+  },
+  {
+    id: 2,
+    tone: 'Bb4',
+    soundDef: 'membrane',
+    events: [0, 0, 1, 0],
+    subdivision: '4n',
+    mute: false
+  },
+  {
+    id: 3,
+    tone: '200',
+    soundDef: 'metal',
+    events: [0, 0, 0, 1],
+    subdivision: '4n',
+    mute: false
+  }
+];
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -7,41 +7,7 @@ import {
   CHANGE_BPM,
   TOGGLE_PLAYING
 } from '../actions/types';
-
-const initialSequences = [
-  {
-    id: 0,
-    tone: '200',
-    soundDef: 'bell',
-    events: [1, 0, 0, 0],
-    subdivision: '4n',
-    mute: false
-  },
-  {
-    id: 1,
-    tone: '200',
-    soundDef: 'conga',
-    events: [0, 1, 0, 0],
-    subdivision: '4n',
-    mute: false
-  },
-  {
-    id: 2,
-    tone: 'Bb4',
-    soundDef: 'membrane',
-    events: [0, 0, 1, 0],
-    subdivision: '4n',
-    mute: false
-  },
-  {
-    id: 3,
-    tone: '200',
-    soundDef: 'metal',
-    events: [0, 0, 0, 1],
-    subdivision: '4n',
-    mute: false
-  }
-];
+import initialSequences from '../data';
 
 // pass this function into a call to Array.prototype.reduce
 const nextId = (maxId, sequence) => Math.max(sequence.id, maxId);
