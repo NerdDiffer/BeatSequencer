df59ffbecc6a1741f8431fb9a189b7d9bf2b910a
Rafael Espinoza <nerddiffer@gmail.com>
2016-09-06 23:32:07 -0700

    Playing with initial state

--- client/src/data.js
+++ client/src/data.js
@@ -1,35 +1,48 @@
 // seed data
 
+const beat1 = [[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+const beat2 = [[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+const beat3 = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]];
+const beat4 = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0]];
+
 export default [
   {
     id: 0,
     tone: '200',
-    soundDef: 'bell',
-    events: [[1, 0, 0, 0], [1, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
+    // soundDef: 'bell',
+    soundDef: 'Cowbell',
+    // events: [[1, 0, 0, 0], [1, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
+    events: beat1,
     subdivision: '4n',
     mute: false
   },
   {
     id: 1,
     tone: '300',
-    soundDef: 'conga',
-    events: [[0, 0, 1, 1], [0, 0, 1, 1], [0, 0, 1, 1], [0, 0, 1, 1]],
+    // soundDef: 'conga',
+    soundDef: 'Clap',
+    // events: [[0, 0, 1, 1], [0, 0, 1, 1], [0, 0, 1, 1], [0, 0, 1, 1]],
+    events: beat2,
     subdivision: '4n',
     mute: false
   },
   {
     id: 2,
     tone: 'Bb4',
-    soundDef: 'membrane',
-    events: [[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0]],
+    // soundDef: 'membrane',
+    soundDef: 'BassDrum',
+    // events: [[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0]],
+    events: beat3,
     subdivision: '4n',
     mute: false
   },
   {
     id: 3,
     tone: '200',
-    soundDef: 'metal',
-    events: [[0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0]],
+    // soundDef: 'metal',
+    soundDef: 'SnareDrum',
+    // events: [[0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0]],
+    events: beat4,
     subdivision: '4n',
     mute: false
   }
