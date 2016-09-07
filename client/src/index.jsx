3892276923571e86568e38866b04b50a8506485c
Rafael Espinoza <nerddiffer@gmail.com>
2016-09-07 16:54:42 -0700

    Replace redux-logger with a chrome extension

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createStore, compose } from 'redux';
 
 import rootReducer from './reducers';
 import App from './components/App'
 
-const middleware = [ logger() ];
-
 const store = createStore(
   rootReducer,
-  applyMiddleware(...middleware)
+  compose(window.devToolsExtension ? window.devToolsExtension() : f => f)
 );
 
 render(
