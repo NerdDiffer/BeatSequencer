1e2d4b189c4883b847e86295951d22d3de30f24d
Rafael Espinoza <nerddiffer@gmail.com>
2016-09-13 10:46:36 -0700

    Refactor generated db models index

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,36 +1,56 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config.json')[env];
-var db        = {};
+const fs           = require('fs');
+const path         = require('path');
+const Sequelize    = require('sequelize');
+
+/* Set up environment */
+const env          = process.env.NODE_ENV || 'development';
+const pathToConfig = path.join(__dirname, '..', 'config.json');
+const config       = require(pathToConfig)[env];
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  const sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  const { database, username, password } = config;
+  const sequelize = new Sequelize(database, username, password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
+/* Build db object */
+const db = {};
+const basename = path.basename(module.filename);
+const helpers = {
+ /**
+  * Returns true if all 3 conditions are true:
+  * - has a `.` in front of the name (file is not 'hidden')
+  * - is not the current file
+  * - ends in the `.js` extension
+  */
+  isModelFile: (file) => {
+    return (file.indexOf('.') !== 0) &&
+           (file !== basename) &&
+           (file.slice(-3) === '.js');
+  },
+  setModelToDb: (file) => {
+    const pathToFile = path.join(__dirname, file);
+    const model = sequelize['import'](pathToFile);
     db[model.name] = model;
-  });
-
-Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+  },
+  associateIfPossible: (modelName) => {
+    if (db[modelName].associate) { db[modelName].associate(db); }
   }
-});
+};
 
+// Select model files & save model references to `db` object
+fs.readdirSync(__dirname)
+  .filter(helpers.isModelFile)
+  .forEach(helpers.setModelToDb);
+
+// Add associations between all models
+const models = Object.keys(db);
+models.forEach(helpers.associateIfPossible);
+
+/* Prepare & export */
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
 module.exports = db;
